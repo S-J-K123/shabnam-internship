579bb5994d1e8244ffaceab2fc42e91fa866f413
S-J-K123 <98359890+S-J-K123@users.noreply.github.com>
2023-01-19 19:12:54 +0000

    test

--- src/pages/Author.jsx
+++ src/pages/Author.jsx
@@ -5,20 +5,9 @@ import { Link, useParams } from "react-router-dom";
 import AuthorImage from "../images/author_thumbnail.jpg";
 import axios from "axios";
 
-
-
 const Author = () => {
   const { id } = useParams();
   const [authors, setAuthors] = useState([]);
-  
-  
-
-
-
-
-
-  
-
 
   // useEffect(() => {
   //   async function getId() {
@@ -29,55 +18,37 @@ const Author = () => {
   //     console.log(data)
 
   //   }
-    
+
   //   getId();
   // }, [id]);
 
-
-
-  
   useEffect(() => {
     async function getId() {
-          const response = await fetch ("https://us-central1-nft-cloud-functions.cloudfunctions.net/authors?author=73855012")
-          const data = await response.json()
-        console.log(data)
-        const author = authors.find(author => author.id === id)
-        console.log(authors)
-
+      const response = await fetch(
+        "https://us-central1-nft-cloud-functions.cloudfunctions.net/authors?author=73855012"
+      );
+      const data = await response.json();
+      setAuthors(data);
+      console.log(data);
+      const author = authors.find((author) => author.id === id);
+      console.log(authors);
     }
-    
+
     getId();
   }, [id]);
 
+  // useEffect(() => {
+  //   async function main() {
+  //     const response = await fetch ("https://us-central1-nft-cloud-functions.cloudfunctions.net/authors?author=73855012")
+  //     const data = await response.json()
+  //     console.log(data)
 
-
-
-   
-
-
-
-// useEffect(() => {
-//   async function main() {
-//     const response = await fetch ("https://us-central1-nft-cloud-functions.cloudfunctions.net/authors?author=73855012")
-//     const data = await response.json()
-//     console.log(data)
-   
-//   }
-//   main()
-// }, [])
-
-
-
-
-
-
-
-
+  //   }
+  //   main()
+  // }, [])
 
   return (
-   
     <div id="wrapper">
-      
       <div className="no-bottom no-top" id="content">
         <div id="top"></div>
 
@@ -91,40 +62,39 @@ const Author = () => {
 
         <section aria-label="section">
           <div className="container">
-
-    <div className="row">
+            <div className="row">
               <div className="col-md-12">
                 <div className="d_profile de-flex">
                   <div className="de-flex-col">
                     <div className="profile_avatar">
                       <img src={AuthorImage} alt="" />
 
-
-{
-  authors?.map((author) => {
-    return(
-            <div className="profile_name">
-                        <h4>
-                          Monica Lewis
-                          <span className="profile_username">@monicaaaa</span>
-                          <span id="wallet" className="profile_wallet">
-                            UDHUHWudhwd78wdt7edb32uidbwyuidhg7wUHIFUHWewiqdj87dy7
-                          </span>
-                          <button id="btn_copy" title="Copy Text">
-                            Copy
-                          </button>
-                        </h4>
-                      </div>
-    )
-  })
-}
+                      {authors?.map((author) => {
+                        return (
+                          <div className="profile_name">
+                            <h4>
+                              Monica Lewis
+                              <span className="profile_username">
+                                @monicaaaa
+                              </span>
+                              <span id="wallet" className="profile_wallet">
+                                UDHUHWudhwd78wdt7edb32uidbwyuidhg7wUHIFUHWewiqdj87dy7
+                              </span>
+                              <button id="btn_copy" title="Copy Text">
+                                Copy
+                              </button>
+                            </h4>
+                          </div>
+                        );
+                      })}
                       <i className="fa fa-check"></i>
-                
                     </div>
                   </div>
                   <div className="profile_follow de-flex">
                     <div className="de-flex-col">
-                      <div className="profile_follower">{authors.followers}</div>
+                      <div className="profile_follower">
+                        {authors.followers}
+                      </div>
                       <Link to="#" className="btn-main">
                         Follow
                       </Link>
@@ -139,7 +109,6 @@ const Author = () => {
                 </div>
               </div>
             </div>
-      
           </div>
         </section>
       </div>
