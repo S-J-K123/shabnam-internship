8ca0bf8cb68d25b798da89ec93c894733459356a
S-J-K123 <98359890+S-J-K123@users.noreply.github.com>
2023-01-22 23:17:14 +0000

    topsellers features added

--- src/pages/Author.jsx
+++ src/pages/Author.jsx
@@ -1,78 +1,52 @@
-import React, { useEffect, useState } from "react";
-import AuthorBanner from "../images/author_banner.jpg";
-import AuthorItems from "../components/author/AuthorItems";
-import { Link, useParams } from "react-router-dom";
-import axios from "axios";
-import { Skeleton } from "@mui/material";
-import AuthorSkeleton from "../components/UI/AuthorSkeleton";
+import React, { useEffect, useState } from "react"
+import AuthorBanner from "../images/author_banner.jpg"
+import AuthorItems from "../components/author/AuthorItems"
+import { Link, useParams } from "react-router-dom"
+import axios from "axios"
+import { Skeleton } from "@mui/material"
+import AuthorSkeleton from "../components/UI/AuthorSkeleton"
 
 const Author = () => {
-  
-
-  const { id } = useParams();
-  const [author, setAuthors] = useState({});
-  const [loading, setLoading] = useState(true);
-  const [showFollow, setShowFollow] = useState(false);
-  const [follow, setFollow] = useState({followers: 0})
-
-
+  const { id } = useParams()
+  const [author, setAuthors] = useState({})
+  const [loading, setLoading] = useState(true)
+  const [showFollow, setShowFollow] = useState(false)
+  const [followers, setFollowers] = useState(0)
 
   const handleFollow = () => {
-    setFollow(prevFollow => {
-        return { ...prevFollow, followers: prevFollow.followers + 1 }
-    });
-}
+    if (showFollow) {
+      setShowFollow(false)
+      setFollowers((currentFollowers) => currentFollowers - 1)
+    } else {
+      setShowFollow(true)
+      setFollowers((currentFollowers) => currentFollowers + 1)
+    }
+  }
 
-
-
-  const loadingCards = [...Array(8)].map((e, i) => (
-    <Skeleton
-      className="skeleton-author"
-      width={"250px"}
-      height={"470px"}
-      key={i}
-    />
-  ));
-
-
-
-  // function increment() {
-  //   setAuthors({ ...author, followers: author.followers + 1 });
-  // }
-
-  // function decrement() {
-  //   setAuthors({ ...author, followers: author.followers - 1 });
-  // }
-
-
+  const loadingCards = [...Array(8)].map((e, i) => <Skeleton style={{ margin: "0 20px" }} width={"250px"} height={"470px"} key={i} />)
 
   useEffect(() => {
-    axios
-      .get(
-        "https://us-central1-nft-cloud-functions.cloudfunctions.net/authors?author=" +
-          id
-      )
-      .then((data) => {
-        console.log(data.data);
-        setAuthors(data.data);
-        setTimeout(() => {
-          setLoading(false);
-        }, 1000);
-      });
-  }, [id]);
+    axios.get("https://us-central1-nft-cloud-functions.cloudfunctions.net/authors?author=" + id).then((data) => {
+      console.log(data.data)
+      setAuthors(data.data)
+      setFollowers(data.data.followers)
+      setTimeout(() => {
+        setLoading(false)
+      }, 1000)
+    })
+    // scroll to the top of the page
+  }, [id])
+
+  // store the number of followers in some state
+  // make a function => if person is being followed, show 'follow' button and decrement followers, else do the opposite
+  // plug in the function to the follow button
 
   return (
     <div id="wrapper">
       <div className="no-bottom no-top" id="content">
         <div id="top"></div>
 
-        <section
-          id="profile_banner"
-          aria-label="section"
-          className="text-light"
-          data-bgimage="url(images/author_banner.jpg) top"
-          style={{ background: `url(${AuthorBanner}) top` }}
-        ></section>
+        <section id="profile_banner" aria-label="section" className="text-light" data-bgimage="url(images/author_banner.jpg) top" style={{ background: `url(${AuthorBanner}) top` }}></section>
 
         <section aria-label="section">
           <div className="container">
@@ -89,9 +63,7 @@ const Author = () => {
                         <div className="profile_name">
                           <h4>
                             {author.authorName}
-                            <span className="profile_username">
-                              {author.tag}
-                            </span>
+                            <span className="profile_username">{author.tag}</span>
                             <span id="wallet" className="profile_wallet">
                               {author.address}
                             </span>
@@ -106,18 +78,10 @@ const Author = () => {
                     </div>
                     <div className="profile_follow de-flex">
                       <div className="de-flex-col">
-                        <div className="profile_follower">
-                          {author.followers} followers
-                        </div>
-                        <Link
-                          to="#"
-                          className="btn-main"
-                          onClick={() => setShowFollow(!showFollow)} 
-                        
-                        >
+                        <div className="profile_follower">{followers} followers</div>
+                        <Link to="#" className="btn-main" onClick={handleFollow}>
                           {!showFollow ? "Follow" : "Unfollow"}
                         </Link>
-                    
                       </div>
                     </div>
                   </div>
@@ -125,23 +89,14 @@ const Author = () => {
               )}
 
               <div className="col-md-12">
-                <div className="de_tab tab_simple">
-                  {loading ? (
-                    loadingCards
-                  ) : (
-                    <AuthorItems
-                      nftCollection={author.nftCollection}
-                      authorImage={author.authorImage}
-                    />
-                  )}
-                </div>
+                <div className={`de_tab tab_simple ${loading ? "skeleton-author" : ""}`}>{loading ? loadingCards : <AuthorItems nftCollection={author.nftCollection} authorImage={author.authorImage} />}</div>
               </div>
             </div>
           </div>
         </section>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Author;
+export default Author
