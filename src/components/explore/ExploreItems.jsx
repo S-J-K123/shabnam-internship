484e35bb2fa7ffb84647513ec4cd53deaf07d829
S-J-K123 <98359890+S-J-K123@users.noreply.github.com>
2023-02-06 14:43:14 +0000

    test

--- src/components/explore/ExploreItems.jsx
+++ src/components/explore/ExploreItems.jsx
@@ -11,35 +11,25 @@ const ExploreItems = () => {
   const { id } = useParams();
   const [posts, setPosts] = useState([]);
   const [visible, setVisible] = useState(4);
-  const [loading, setLoading] = useState(true)
+  const [loading, setLoading] = useState(true);
 
   const loadMore = () => {
     setVisible((visible) => visible + 4);
   };
 
-
   const handleFilterChange = (event) => {
-   setLoading(true)
+    setLoading(true);
 
-   let filterURL =   `https://us-central1-nft-cloud-functions.cloudfunctions.net/explore?filter=${event.target.value}`
-   if(event.target.value === "default") {
-    filterURL = "https://us-central1-nft-cloud-functions.cloudfunctions.net/explore/"
-   }
-   axios.get (filterURL).then(data =>{
-    setPosts(data.data)
-    setLoading(false)
-   })
+    let filterURL = `https://us-central1-nft-cloud-functions.cloudfunctions.net/explore?filter=${event.target.value}`;
+    if (event.target.value === "default") {
+      filterURL =
+        "https://us-central1-nft-cloud-functions.cloudfunctions.net/explore/";
+    }
+    axios.get(filterURL).then((data) => {
+      setPosts(data.data);
+      setLoading(false);
+    });
   };
-
- 
- 
-    
-
-
-  
-
-
-
 
   useEffect(() => {
     async function exploreId() {
@@ -51,8 +41,8 @@ const ExploreItems = () => {
           console.log(data.data);
           setPosts(data.data);
           setTimeout(() => {
-             setLoading(false)
-          }, 2000)
+            setLoading(false);
+          }, 2000);
         });
     }
     exploreId();
@@ -73,79 +63,71 @@ const ExploreItems = () => {
         </select>
       </div>
 
-
-
       {posts.slice(0, visible).map((post, index) => (
         <div
           key={index}
           className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
           style={{ display: "block", backgroundSize: "cover" }}
         >
-
-          {
-            loading ? (
-              <Skeleton width={"300px"}
-              height={"500px"}/>
-            ) :
-            (
-                 <div className="nft__item">
-            <div className="author_list_pp">
-              <Link
-                to={`/author/${post.authorId}`}
-                data-bs-toggle="tooltip"
-                data-bs-placement="top"
-              >
-                <img className="lazy" src={post.authorImage} alt="" />
-                <i className="fa fa-check"></i>
-              </Link>
-            </div>
-            {post.expiryDate ? (
-              <div className="de_countdown">
-                {" "}
-                <Timer expiryDate={post.expiryDate} />{" "}
+          {loading ? (
+            <Skeleton width={"300px"} height={"500px"} />
+          ) : (
+            <div className="nft__item">
+              <div className="author_list_pp">
+                <Link
+                  to={`/author/${post.authorId}`}
+                  data-bs-toggle="tooltip"
+                  data-bs-placement="top"
+                >
+                  <img className="lazy" src={post.authorImage} alt="" />
+                  <i className="fa fa-check"></i>
+                </Link>
               </div>
-            ) : null}
+              {post.expiryDate ? (
+                <div className="de_countdown">
+                  {" "}
+                  <Timer expiryDate={post.expiryDate} />{" "}
+                </div>
+              ) : null}
 
-            <div className="nft__item_wrap">
-              <div className="nft__item_extra">
-                <div className="nft__item_buttons">
-                  <button>Buy Now</button>
-                  <div className="nft__item_share">
-                    <h4>Share</h4>
-                    <a href="" target="_blank" rel="noreferrer">
-                      <i className="fa fa-facebook fa-lg"></i>
-                    </a>
-                    <a href="" target="_blank" rel="noreferrer">
-                      <i className="fa fa-twitter fa-lg"></i>
-                    </a>
-                    <a href="">
-                      <i className="fa fa-envelope fa-lg"></i>
-                    </a>
+              <div className="nft__item_wrap">
+                <div className="nft__item_extra">
+                  <div className="nft__item_buttons">
+                    <button>Buy Now</button>
+                    <div className="nft__item_share">
+                      <h4>Share</h4>
+                      <a href="" target="_blank" rel="noreferrer">
+                        <i className="fa fa-facebook fa-lg"></i>
+                      </a>
+                      <a href="" target="_blank" rel="noreferrer">
+                        <i className="fa fa-twitter fa-lg"></i>
+                      </a>
+                      <a href="">
+                        <i className="fa fa-envelope fa-lg"></i>
+                      </a>
+                    </div>
                   </div>
                 </div>
+                <Link to={`/item-details/${post.nftId}`}>
+                  <img
+                    src={post.nftImage}
+                    className="lazy nft__item_preview"
+                    alt=""
+                  />
+                </Link>
               </div>
-              <Link to={`/item-details/${post.nftId}`}>
-                <img
-                  src={post.nftImage}
-                  className="lazy nft__item_preview"
-                  alt=""
-                />
-              </Link>
-            </div>
-            <div className="nft__item_info">
-              <Link to="/item-details">
-                <h4>{post.title}</h4>
-              </Link>
-              <div className="nft__item_price">{post.price}</div>
-              <div className="nft__item_like">
-                <i className="fa fa-heart"></i>
-                <span>{post.likes}</span>
+              <div className="nft__item_info">
+                <Link to="/item-details">
+                  <h4>{post.title}</h4>
+                </Link>
+                <div className="nft__item_price">{post.price}</div>
+                <div className="nft__item_like">
+                  <i className="fa fa-heart"></i>
+                  <span>{post.likes}</span>
+                </div>
               </div>
             </div>
-          </div>
-            )
-          }
-       
+          )}
         </div>
       ))}
       <div className="col-md-12 text-center">
--- src/components/home/Landing.jsx
+++ src/components/home/Landing.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import NFT from "../../images/nft.png";
 import backgroundImage from "../../images/bg-shape-1.jpg";
 import { Link } from "react-router-dom";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const Landing = () => {
+  useEffect(() => {
+    AOS.init();
+  }, []);
+
   return (
     <section
       id="section-hero"
@@ -17,13 +23,27 @@ const Landing = () => {
           <div className="row align-items-center">
             <div className="col-md-6">
               <div className="spacer-single"></div>
-              <h6>
-                <span className="text-uppercase id-color-2">
-                  Ultraverse Market
-                </span>
-              </h6>
-              <div className="spacer-10"></div>
-              <h1>Create, sell or collect digital items.</h1>
+
+              <div
+                data-aos="fade-up"
+                data-aos-offset="200"
+                data-aos-delay="50"
+                data-aos-duration="1000"
+                data-aos-easing="ease-in-out"
+                data-aos-mirror="true"
+                data-aos-once="false"
+                data-aos-anchor-placement="top-center"
+              >
+                <h6>
+                  <span className="text-uppercase id-color-2">
+                    Ultraverse Market
+                  </span>
+                </h6>
+                <div className="spacer-10"></div>
+
+                <h1>Create, sell or collect digital items.</h1>
+              </div>
+
               <p className="lead">
                 Unit of data stored on a digital ledger, called a blockchain,
                 that certifies a digital asset to be unique and therefore not
